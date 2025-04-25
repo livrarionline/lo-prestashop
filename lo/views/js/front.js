b34551c1ad211b325975fd9a5be186f3b5669f48
Adrian <adrian@plationline.eu>
2025-04-25 17:46:16 +0300

    initial commit for prestashop 8

--- lo/views/js/front.js
+++ lo/views/js/front.js
@@ -12,16 +12,17 @@
  * @license   LICENSE.txt
  */
 
-$(function() {
-    $(document).ready(function() {
+$(function () {
+    $(document).ready(function () {
         $('.btn-smart-locker').fancybox({
             width: '80%',
             autoSize: false,
-            afterShow: function() {
+            afterShow: function () {
                 LoGoogleMap();
             },
-            afterClose: function() {
+            afterClose: function () {
                 LoCheckSmartLockersCarrier();
+                $('button[name="confirmDeliveryOption"]').trigger('click');
             }
         });
         LoCheckSmartLockersCarrier();
@@ -30,18 +31,22 @@ $(function() {
             LoCheckSmartLockersCarrier();
         });
     });
+
+    $('#lo_smart_lockers_area').appendTo($('label[for="delivery_option_' + lo_lockers_carrier + '"]'));
+
     $(document).on('change', 'select[name="lo_state"]', LoCheckCities);
     $(document).on('change', 'select[name="lo_city"]', LoCheckLockers);
     $(document).on('change', 'select[name="lo_locker"]', LoSaveCartLocker);
 
     $(document).on('change', 'input.delivery_option_radio, .delivery-option input[type="radio"]', LoCheckSmartLockersCarrier);
 
-    if($(document).find('#lockers_gmap.only_selected_locker').length) {
+    if ($(document).find('#lockers_gmap.only_selected_locker').length) {
         LoGoogleMap(lo_selected_locker);
     }
 
 
 });
+
 function LoCheckSmartLockersCarrier() {
     if ($(document).find('input.delivery_option_radio:checked').length) {
         id_selected_carrier = $(document).find('input.delivery_option_radio:checked').val();
@@ -67,7 +72,7 @@ function LoCheckSmartLockersCarrier() {
         if (typeof lo_lockers_carrier !== 'undefined' && id_selected_carrier == lo_lockers_carrier) {
             $('#lo_smart_lockers_area').slideDown();
             if (!lo_selected_locker) {
-                $('.btn-smart-locker').trigger('click');
+                // $('.btn-smart-locker').trigger('click');
                 $('[name="confirmDeliveryOption"]').prop('disabled', true);
                 $('.payment_module > a').css('pointerEvents', 'none').css('opacity', 0.5);
             } else {
@@ -75,20 +80,21 @@ function LoCheckSmartLockersCarrier() {
                 $('.payment_module > a').css('pointerEvents', 'auto').css('opacity', 1);
             }
         } else {
-            $('.btn-smart-locker').slideUp();
+            $('#lo_smart_lockers_area').slideUp();
             $('[name="confirmDeliveryOption"]').prop('disabled', false);
             $('.payment_module > a').css('pointerEvents', 'auto').css('opacity', 1);
         }
     }
 }
+
 function LoCheckCities() {
     var state = $('.fancybox-inner select[name="lo_state"]').val();
     if (state) {
         $('.fancybox-inner select[name="lo_city"]').find('option[value!="0"]').remove();
 
         var options_html = "";
-        for(i in lockers_data_array[state]) {
-            options_html += '<option value="'+i+'">'+i+'</option>';
+        for (i in lockers_data_array[state]) {
+            options_html += '<option value="' + i + '">' + i + '</option>';
         }
 
         $('.fancybox-inner select[name="lo_city"]').append(options_html);
@@ -98,6 +104,7 @@ function LoCheckCities() {
         $('.fancybox-inner select[name="lo_city"]').change();
     }
 }
+
 function LoCheckLockers() {
     var state = $('.fancybox-inner select[name="lo_state"]').val();
     var city = $('.fancybox-inner select[name="lo_city"]').val();
@@ -105,8 +112,8 @@ function LoCheckLockers() {
         $('.fancybox-inner select[name="lo_locker"]').find('option[value!="0"]').remove();
 
         var options_html = "";
-        for(i in lockers_data_array[state][city]) {
-            options_html += '<option '+(lockers_data_array[state][city][i]['dp_active'] == 10 || lockers_data_array[state][city][i]['dp_active'] <= 0?'disabled':'')+' value="'+lockers_data_array[state][city][i]['dp_id']+'">'+lockers_data_array[state][city][i]['dp_denumire']+'</option>';
+        for (i in lockers_data_array[state][city]) {
+            options_html += '<option ' + (lockers_data_array[state][city][i]['dp_active'] == 10 || lockers_data_array[state][city][i]['dp_active'] <= 0 ? 'disabled' : '') + ' value="' + lockers_data_array[state][city][i]['dp_id'] + '">' + lockers_data_array[state][city][i]['dp_denumire'] + '</option>';
         }
 
         $('.fancybox-inner select[name="lo_locker"]').append(options_html);
@@ -116,12 +123,13 @@ function LoCheckLockers() {
         $('.fancybox-inner select[name="lo_locker"]').change();
     }
 }
+
 function LoCheckSelectedLocker(selected_locker) {
     if (selected_locker) {
         lo_selected_locker = selected_locker;
     }
     if (typeof lo_selected_locker !== 'undefined' && lo_selected_locker) {
-        for(var i in lockers_array) {
+        for (var i in lockers_array) {
             if (lockers_array[i]['dp_id'] == lo_selected_locker) {
                 $('.fancybox-inner select[name="lo_state"]').val(lockers_array[i]['dp_judet']).change();
                 $('.fancybox-inner select[name="lo_city"]').val(lockers_array[i]['dp_oras']).change();
@@ -151,7 +159,7 @@ function LoSaveCartLocker() {
                 token: lo_ajax_token,
             },
         })
-            .done(function(json) {
+            .done(function (json) {
                 if (typeof json.error !== 'undefined') {
                     alert(json.error);
                 } else {
@@ -183,7 +191,7 @@ function LoSaveCartLocker() {
                 token: lo_ajax_token,
             },
         })
-            .done(function(json) {
+            .done(function (json) {
                 if (typeof json.error !== 'undefined') {
                     alert(json.error);
                 } else {
@@ -194,16 +202,6 @@ function LoSaveCartLocker() {
                         $('#carrier_area').replaceWith(json.carriers_tpl);
                         if (!!$.prototype.uniform)
                             $("select.form-control,input[type='radio'],input[type='checkbox']").not(".not_uniform").uniform();
-                    } else {
-                        if (json.get_from_window_href) {
-                            $.ajax({
-                                url: window.location.href,
-                                cache: false,
-                            })
-                                .done(function(html) {
-                                    $(document).find('#checkout-delivery-step').replaceWith($(html).find('#checkout-delivery-step'));
-                                });
-                        }
                     }
                 }
             });
